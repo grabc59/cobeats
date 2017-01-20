688dc75d7ead16f767d26cbb72dc509abb372b8a
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-20 14:43:20 -0700

    Add test suite requires

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,1 +1,16 @@
 'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const knex = require('../src/server/db/knex')
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('TESTING test skeleton', () => {
+  // TEST SKELETON
+});
