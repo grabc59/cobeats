70e0b6eb26efb13684de877adb0187c64bfad495
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-20 14:55:00 -0700

    Add GET all test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,12 +5,22 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-const knex = require('../src/server/db/knex')
+const knex = require('../src/server/db/knex');
 
 const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('TESTING test skeleton', () => {
-  // TEST SKELETON
+describe('API routes', () => {
+
+  describe('GET /admin/users', () => {
+    it('should return all users', (done) => {
+      chai.request(server)
+      .get('/admin/users')
+      .end((err, res) => {
+        res.should.have.status(200);
+      });
+    });
+  });
+
 });
