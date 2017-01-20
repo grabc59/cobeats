2c1b1318641c6025eb811ef6597210128bf56934
Matthew Gordon <laxredemption5525@gmail.com>
2017-01-20 15:05:58 -0700

    Configure logger settings for development/production

--- app.js
+++ app.js
@@ -9,8 +9,42 @@ const admin = require('./src/server/routes/admin');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+
 app.get('/', (req, res, next) => {
   res.send('Hello World! From coBeats');
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handlers
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handlers
+// no stacktrace leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 app.listen(port, () => {
