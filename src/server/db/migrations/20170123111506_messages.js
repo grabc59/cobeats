d20166251947934f2f031075a39110ac5435f42a
Christopher Grabski <grabc59@gmail.com>
2017-01-23 11:29:35 -0700

    customize the messages migration file. Tested both successfully. Still need to customize and test seeds

--- src/server/db/migrations/20170123111506_messages.js
+++ src/server/db/migrations/20170123111506_messages.js
@@ -1,8 +1,15 @@
-
+'use strict';
 exports.up = function(knex, Promise) {
-
+  return knex.schema.createTable('messages', (table) => {
+    table.increments().notNullable();
+    table.string('content');
+    // user id of the message sender
+    table.integer('sender').notNullable().references('users.id');
+    // created_at, updated_at
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable('messages');
 };
