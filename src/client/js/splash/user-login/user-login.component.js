7cfe5de02636de2067ab3603ae8cf00b1ae9663f
Christopher Grabski <grabc59@gmail.com>
2017-01-23 22:15:50 -0700

    clean up comments in the user login component

--- src/client/js/splash/user-login/user-login.component.js
+++ src/client/js/splash/user-login/user-login.component.js
@@ -5,7 +5,7 @@
     .component('userLogin', {
       controller: controller,
       templateUrl: 'js/splash/user-login/user-login.template.html'
-    })
+    });
 
     controller.$inject = ['$http'];
 
@@ -14,17 +14,12 @@
       console.log('user log in component controller log');
 
       vm.submitUsername = function(username) {
-        // console.log('username: 'username);
-
-        //set the username in localstorage
         localStorage.username = username;
-
         // send a post to the users table
         // format the username like an object so it will be stringified
         $http.post('users-route/', {username: username})
           .then(function (response) {
             vm.postResponse = response.data;
-            // console.log('success');
           });
       };
 
