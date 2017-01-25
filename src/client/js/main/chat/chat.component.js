2a4d5a0f84cb380554f940cd39171288fd58c15d
Christopher Grabski <grabc59@gmail.com>
2017-01-24 23:11:05 -0700

    current user is highlighted in chat

--- src/client/js/main/chat/chat.component.js
+++ src/client/js/main/chat/chat.component.js
@@ -15,11 +15,26 @@
     function controller($http, $state, socket) {
       const vm = this;
       console.log('chat component controller log');
+
+      ////// INIT
       vm.$onInit = function() {
-        // socket.emit('new user', username.val(), function(data) {
+        vm.current_user = localStorage.username
+        vm.connected_users = [];
         socket.emit('new user', localStorage.username, function(data) {
             console.log('new user log: ', data, localStorage.username);
         });
       }
+
+      ////// NEW USER NOTIFICATION
+      socket.on('new user notification', function(data) {
+        // vm.connected_users.push(data);
+        //$messageHistory.append('<div class="well"><strong>' + data + '</strong> has joined the chat');
+      });
+
+      ////// POPULATE USERS ONLINE
+      socket.on('get users', function(data){
+        vm.connected_users = data
+        console.log(vm.connected_users);
+      });
     }
 }());
