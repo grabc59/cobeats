34569a4d8818327e083a8778bfeec3d70641dc08
Christopher Grabski <grabc59@gmail.com>
2017-01-25 12:49:08 -0700

    point electron to heroku deploy

--- main.js
+++ main.js
@@ -13,11 +13,12 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, '/src/client/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  // win.loadURL(url.format({
+  //   pathname: path.join(__dirname, '/src/client/index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
+  win.loadURL('https://cobeats.herokuapp.com');
 
   // Open the DevTools.
   win.webContents.openDevTools()
