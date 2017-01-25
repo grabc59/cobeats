6d44bfba48c1c96d61b95ec84aaa3110574514f8
Christopher Grabski <grabc59@gmail.com>
2017-01-24 20:45:42 -0700

    add socket event listeners

--- app.js
+++ app.js
@@ -72,7 +72,59 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(port, () => {
+
+
+
+//////////////////////////////////////
+//////////      SOCKET SERVER
+//////////////////////////////////////
+io.sockets.on('connection', function(socket) {
+
+  //////////// NEW CONNECTION EVENT
+  connections.push(socket);
+  // informational logs
+  console.log('Connected: %s sockets connected', connections.length);
+  console.log(connections.length);
+
+  //////////// DISCONNECT EVENT
+  socket.on('disconnect', function(data) {
+    if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
+    connections.splice(connections.indexOf(socket),1);
+    console.log('disconnected: %s sockets connected', connections.length);
+  });
+
+  //////////// SEND MESSAGE EVENT
+  socket.on('send message', function(data) {
+    console.log(data);
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  //////////// NEW USER EVENT
+  socket.on('new user', function(data, callback) {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+    io.sockets.emit('new user notification', socket.username);
+  });
+
+  //////////// GET USERNAMES
+  function updateUsernames() {
+    io.sockets.emit('get users', users);
+  }
+
+  //////////// USER IS TYPING EVENT
+  socket.on("typing", function(data) {
+    io.sockets.emit('isTyping', {
+      isTyping: data,
+      person: socket.username,
+    });
+  });
+});
+
+server.listen(port, () => {
   console.log(`CoBeats is running on http://localhost:${port} ...`);
 });
 
