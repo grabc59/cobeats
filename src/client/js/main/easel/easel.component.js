b8c879c8f52ad1a4924454a9d3201e8f89b1d047
Ariana Clark <ariana.clark@gmail.com>
2017-01-25 19:54:48 -0700

    color selection/application added to controller

--- src/client/js/main/easel/easel.component.js
+++ src/client/js/main/easel/easel.component.js
@@ -1,12 +1,31 @@
-(function() {
+( function() {
   'use strict';
   angular
-    .module('app')
-    .component('easel', {
+    .module( 'app' )
+    .component( 'easel', {
       controller: controller,
       templateUrl: 'js/main/easel.template.html'
-    })
-    function controller() {
-      const vm = this;
+    } )
+
+  function controller() {
+    const vm = this;
+
+    vm.selectColor = selectColor;
+    vm.applyColor = applyColor;
+    vm.currentColor = '#FFFFFF'
+    vm.pixelArray = pixelArray;
+
+    function selectColor( event ) {
+      vm.currentColor = event.target.style.backgroundColor;
+      document.getElementById( 'current-color' ).style.backgroundColor = vm.currentColor;
     }
-}());
+
+    function applyColor( event ) {
+      event.target.style.backgroundColor = vm.currentColor;
+    }
+
+    function pixelArray() {
+      return new Array( 48 );
+    }
+  }
+}() );
