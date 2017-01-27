1cb080b80257ebb1737f0871cde54d2c165129f5
Christopher Grabski <grabc59@gmail.com>
2017-01-26 23:09:46 -0700

    adjusting the online users and messages  below the easel

--- src/client/js/main/easel/easel.component.js
+++ src/client/js/main/easel/easel.component.js
@@ -5,7 +5,22 @@
     .component( 'easel', {
       controller: controller,
       templateUrl: 'js/main/easel/easel.template.html'
-    } )
+    })
+    // .run(["$templateCache", function ($templateCache) {
+
+    
+//     angular.module("template/popover/popover.html", []).run(["$templateCache", function ($templateCache) {
+//     $templateCache.put("template/popover/popover.html",
+//       "<div class=\"popover {{placement}}\" ng-class=\"{ in: isOpen(), fade: animation() }\">\n" +
+//       "  <div class=\"arrow\"></div>\n" +
+//       "\n" +
+//       "  <div class=\"popover-inner\">\n" +
+//       "      <h3 class=\"popover-title\" ng-bind-html=\"title | unsafe\" ng-show=\"title\"></h3>\n" +
+//       "      <div class=\"popover-content\"ng-bind-html=\"content | unsafe\"></div>\n" +
+//       "  </div>\n" +
+//       "</div>\n" +
+//       "");
+// }]);
 
   controller.$inject = [ 'socket' ];
 
